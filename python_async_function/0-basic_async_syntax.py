af5374a06b06bb2b6df68a053839a5b0405e5fbf
FelixBesancon <f.besancon@hotmail.fr>
2026-04-08 09:35:59 +0200

    Add asynchronous coroutine to return a random delay

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines an asynchronous coroutine that takes in an integer argument"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """Returns a random delay between 0 and max_delay (included)"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
