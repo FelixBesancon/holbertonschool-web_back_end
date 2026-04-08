d8b5606a045d04d64cb63e665f88d80602077f44
FelixBesancon <f.besancon@hotmail.fr>
2026-04-08 12:15:25 +0200

    Refactor async_generator to specify return type and simplify yield statement

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,15 +3,15 @@
 
 
 import asyncio
+from typing import AsyncGenerator
 import random
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     The coroutine will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10. Use the random module.
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        result = random.uniform(0, 10)
-        yield result
+        yield random.uniform(0, 10)
