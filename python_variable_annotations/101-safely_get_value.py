c98fd1367601cc051222cc6c7237c5ea49534fb2
FelixBesancon <f.besancon@hotmail.fr>
2026-04-09 10:45:44 +0200

    Add safely_get_value function to retrieve values from a dictionary with a default fallback

---
+++ python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Defines a type-annotated function safely_get_value."""
+
+
+from typing import Mapping, Any, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None
+) -> Union[Any, T]:
+    """
+    Returns the value corresponding to a key in a dictionary if it exists,
+    otherwise returns a default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
