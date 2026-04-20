6ca53aa92fdcb71639035fb4d10dc38d8e417362
FelixBesancon <f.besancon@hotmail.fr>
2026-04-20 09:36:24 +0200

    Add index_range function for pagination and create README.md

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Defines a function that returns a tuple of size two containing
+a start index and an end index corresponding to the range of indexes
+to return in a list for pagination.
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index
+    and an end index.
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+    Returns:
+        Tuple[int, int]: A tuple containing the start index and end index.
+    """
+    if page < 1 or page_size <= 0:
+        return (0, 0)
+    return (
+        (page - 1) * page_size,
+        page * page_size)
