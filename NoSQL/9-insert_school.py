3fdf14ec6295c042caab5812633ba0c5b82a6e9e
FelixBesancon <f.besancon@hotmail.fr>
2026-04-22 14:29:32 +0200

    Update module docstring for insert_school function to clarify its purpose

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
-Module that inserts a new document in a collection
-based on kwargs and returns the new _id
+Module that defines the function insert_school.
 """
 
 
 def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs
+    and returns the new _id.
+    """
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
