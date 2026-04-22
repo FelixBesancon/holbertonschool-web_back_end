d9e34a9ee11328c43d60d20039d8508730b68902
FelixBesancon <f.besancon@hotmail.fr>
2026-04-22 14:42:07 +0200

    Add update_topics function to update school document topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that defines the function update_topics.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    to the new topics list.
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
