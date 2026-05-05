f96771ccf22f9bd3a1c490cbeb8c13238847a55a
FelixBesancon <f.besancon@hotmail.fr>
2026-05-05 10:18:32 +0200

    fix: format function declarations for consistency and add getStudentsByLocation function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,3 @@
-export default function getListStudentIds(listStudents) {
+export default function getListStudentIds (listStudents) {
   return Array.isArray(listStudents) ? listStudents.map(student => student.id) : [];
 }
