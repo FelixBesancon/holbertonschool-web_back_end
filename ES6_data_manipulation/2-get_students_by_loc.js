c83053f9e4b986494d8386e40423b9044b040158
FelixBesancon <f.besancon@hotmail.fr>
2026-05-05 10:30:48 +0200

    feat: add getStudentIdsSum function to calculate the sum of student IDs and improve getStudentsByLocation function readability

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation (listStudents, city) {
-  return Array.isArray(listStudents) ? listStudents.filter(s => s.location === city) : [];
+  return Array.isArray(listStudents) ? listStudents.filter(student => student.location === city) : [];
 }
---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,3 @@
+export default function getStudentIdsSum (listStudents) {
+  return Array.isArray(listStudents) ? listStudents.reduce ((acc, student) => acc + student.id, 0) : 0;
+}
