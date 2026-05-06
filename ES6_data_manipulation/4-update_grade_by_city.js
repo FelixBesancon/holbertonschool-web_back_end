aacb154e144ac7664dab49b68e57978e08910a66
FelixBesancon <f.besancon@hotmail.fr>
2026-05-06 10:57:10 +0200

    refactor: simplify return statements in updateStudentGradeByCity and cleanSet functions for improved readability

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,9 @@
 export default function updateStudentGradeByCity (listStudents, city, newGrades) {
-  return Array.isArray(listStudents)
-    ? (
-        listStudents
-          .filter(student => student.location === city)
-          .map(student => ({
-            ...student,
-            grade: newGrades.find(grade => grade.studentId === student.id)?.grade ?? 'N/A'
-          }))
-      )
-    : [];
+  if (!Array.isArray(listStudents)) return [];
+  return listStudents
+    .filter(student => student.location === city)
+    .map(student => ({
+      ...student,
+      grade: newGrades.find(grade => grade.studentId === student.id)?.grade ?? 'N/A'
+    }));
 }
--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,8 @@
 export default function cleanSet (set, startString) {
-  return !startString
-    ? ''
-    : (
-        Array
-          .from(set)
-          .filter(string => string.startsWith(startString))
-          .map(string => string.slice(startString.length))
-          .join('-')
-      );
+  if (!startString) return '';
+  return Array
+    .from(set)
+    .filter(string => string.startsWith(startString))
+    .map(string => string.slice(startString.length))
+    .join('-');
 }
