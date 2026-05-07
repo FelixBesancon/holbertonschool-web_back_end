06e1a31fdaf887f63ea07efb2eb66bf2144a6889
FelixBesancon <f.besancon@hotmail.fr>
2026-05-07 11:41:39 +0200

    refactor: improve result mapping in handleProfileSignup for clarity

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,5 +4,8 @@ import uploadPhoto from './5-photo-reject.js';
 export default function handleProfileSignup (firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(result => result.map(r => ({ status: r.status, value: r.value || r.reason })));
+    .then(result => result.map(element => ({
+      status: element.status,
+      value: element.status === "fulfilled" ? element.value : element.reason
+    })));
 }
