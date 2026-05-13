c2c2ded8cb456f61081a53cb63895db8e7cf7062
FelixBesancon <f.besancon@hotmail.fr>
2026-05-13 10:51:46 +0200

    feat: add HTTP server and countStudents function to handle student data

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('\nCannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      const keys = lines[0].split(',');
+      const students = lines.slice(1)
+        .filter((line) => line.trim() !== '')
+        .map((line) => {
+          const obj = {};
+          line.split(',').forEach((value, index) => {
+            obj[keys[index]] = value;
+          });
+          return obj;
+        });
+
+      let response = `\nNumber of students: ${students.length}\n`;
+
+      const fields = [...new Set(students.map((student) => student.field))];
+
+      for (const field of fields) {
+        const fieldStudents = students
+          .filter((student) => student.field === field)
+          .map((student) => student.firstname);
+        response += `Number of students in ${field}: ${fieldStudents.length}. List: ${fieldStudents.join(', ')}\n`;
+      }
+
+      resolve(response.trimEnd());
+    });
+  });
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2])
+    .then((students) => {
+      res.send(`This is the list of our students${students}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
