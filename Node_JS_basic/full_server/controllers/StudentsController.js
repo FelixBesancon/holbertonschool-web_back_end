0889bb3bed785308cd50a69cd64d264510b883d6
FelixBesancon <f.besancon@hotmail.fr>
2026-05-13 15:30:32 +0200

    fix: standardize error response message for database loading

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -14,7 +14,7 @@ export default class StudentsController {
         response.send(result.trimEnd());
       })
       .catch((err) => {
-        response.status(500).send(err.message);
+        response.status(500).send('Cannot load the database');
       });
   }
 
@@ -31,7 +31,7 @@ export default class StudentsController {
         response.send(result.trimEnd());
       })
       .catch((err) => {
-        response.status(500).send(err.message);
+        response.status(500).send('Cannot load the database');
       });
   }
 }
